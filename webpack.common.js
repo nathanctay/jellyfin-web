e265cee03376ffee58ada23eb099593464ba8633
nathanctay <63444651+nathanctay@users.noreply.github.com>
2026-09-09 20:46:29 -0600

    fixed exit

--- webpack.common.js
+++ webpack.common.js
@@ -72,7 +72,8 @@ const config = {
             __PACKAGE_JSON_VERSION__: JSON.stringify(packageJson.version),
             __USE_SYSTEM_FONTS__: !!JSON.parse(process.env.USE_SYSTEM_FONTS || '0'),
             __WEBPACK_SERVE__: !!JSON.parse(process.env.WEBPACK_SERVE || '0'),
-            __SERVER_URL__: JSON.stringify(process.env.JF_SERVER_URL || '')
+            __SERVER_URL__: JSON.stringify(process.env.JF_SERVER_URL || ''),
+            __MULTIPLAYER_SERVER__: JSON.stringify(process.env.MULTIPLAYER_SERVER || '')
         }),
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
